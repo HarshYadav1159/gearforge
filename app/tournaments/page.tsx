ed512dedf2deb1b2193ccc26b7e5983653fec799
HarshYadav <hy717330@gmail.com>
2025-07-02 21:17:13 +0530

    Tournaments Card

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,5 +1,18 @@
-function Tournaments(){
-    return (<>To be implemented</>)
-}
+import React from "react";
+import LiveTournaments from "./sections/LiveTournaments";
+import UpcomingTournaments from "./sections/UpcomingTournaments";
 
-export default Tournaments
+const Tournaments: React.FC = () => {
+  return (
+    <div className="min-h-screen overflow-y-auto bg-zinc-900">
+      <div className="flex flex-col h-full py-6 space-y-8">
+        <LiveTournaments />
+        <UpcomingTournaments />
+      </div>
+    </div>
+  );
+};
+
+export default Tournaments;
+
+
